e44d138a07be5fbca4cd823abcb434435a10acc7
sky <skysth3limit@pm.me>
2024-03-27 22:55:53 -0400

    extra check + reasons

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.post("/add", async (req, res) => {
   let pathRegex = /[a-z 0-9 -]\/{1}.*/;
   let characterRegex = /[^a-zA-Z 0-9 - . / : ]/g;
   console.log(req.body);
-  if(req.body.domain && req.body.domain.length < 100) {
+  let reason;
+  if(req.body.domain && req.body.domain.length < 70) {
     console.log("domain exists and is short enough");
     let link = req.body.domain;
     link = link.replace(characterRegex, "");
@@ -36,22 +37,36 @@ app.post("/add", async (req, res) => {
       console.log("running fetch");
       let dns = await (await fetch(`https://api.api-ninjas.com/v1/dnslookup?domain=${link}`, {headers:{"Origin": "https://api-ninjas.com"}})).json();
       console.log("fetch completed");
-      if(dns[0]) {
-        console.log("data found");
-        if(dns[0].record_type == "A") {
-          console.log("dns[0] is a record");
-          if(dns[0].value == "5.161.118.69") {
-            console.log("correct domain");
-            data.push(link);
-            fs.writeFileSync("./domains.json", JSON.stringify(data));
-            res.status(200).end();
-            return;
+      if(data.indexOf(link) < 0) {
+        if(dns[0]) {
+          console.log("data found");
+          if(dns[0].record_type == "A") {
+            console.log("dns[0] is a record");
+            if(dns[0].value == "5.161.118.69") {
+              console.log("correct domain");
+              data.push(link);
+              fs.writeFileSync("./domains.json", JSON.stringify(data));
+              res.status(200).end();
+              return;
+            } else {
+              reason = "The DNS was not set to the right IP.";
+            }
+          } else {
+            reason = "Did not detect an A record.";
           }
+        } else {
+          reason = "No DNS records were found.";
         }
+      } else {
+        reason = "Link has already been added.";
       }
+    } else {
+      reason = "Link wasn't detected as a valid link.";
     }
+  } else {
+    reason = "No link or link was too long. (should not be possible, contact @skysthelimit.dev immediately)";
   }
-  res.status(404).end();
+  res.status(404).send(reason);
 })
 
 app.use(function(req, res) {
